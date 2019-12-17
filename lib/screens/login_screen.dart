80b5c7254ddd48eed5240338dd318cd9b6cb831c
Subhi Pandey <subhi2012cpi@gmail.com>
2019-12-18 00:24:08 +0530

    Update login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -7,6 +7,18 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
 final _formKey = GlobalKey<FormState>();
+String _email, _password;
+
+_submit(){
+
+  if (_formKey.currentState.validate()){
+    _formKey.currentState.save();
+    print(_email);
+    print(_password);
+  }
+}
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -26,12 +38,45 @@ final _formKey = GlobalKey<FormState>();
               key: _formKey,
               child: Column(mainAxisSize: MainAxisSize.min,
               children: <Widget>[
-                TextFormField(
-                  decoration:InputDecoration(labelText: 'Email'),
-                  validator: (input) => !input.contains('@')
-                  ? 'Please enter a valid email'
-                            : null,
-                  )
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                        horizontal: 30.0,
+                        vertical: 10.0,
+                      ),
+                  child: TextFormField(
+                    decoration:InputDecoration(labelText: 'Email'),
+                    validator: (input) => !input.contains('@')
+                    ? 'Please enter a valid email': null,
+                    onSaved: (input) => _email = input,
+                    ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                        horizontal: 30.0,
+                        vertical: 10.0,
+                      ),
+                  child: TextFormField(
+                    decoration:InputDecoration(labelText: 'Password'),
+                    validator: (input) => input.length < 6
+                    ? 'Password should be of 6 Characters': null,
+                    onSaved: (input) => _email = input,
+                    obscureText: true,
+                    ),
+                ),
+                SizedBox(height: 20.0),
+                Container(
+                  width: 250,
+                  child: FlatButton(
+                  onPressed: _submit, 
+                  color: Colors.blue,
+                  padding: EdgeInsets.all(10.0),
+                  child: Text('Login', style: TextStyle(
+                  color: Colors.white, 
+                  fontSize: 10.0
+                  ),
+                  ),
+                  ),
+                ),
               ],
               )
               
