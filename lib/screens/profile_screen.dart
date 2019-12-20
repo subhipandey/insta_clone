3adb75acae6a5a48d2b8dc340b3fe48b5bdced2a
Subhi Pandey <subhi2012cpi@gmail.com>
2019-12-20 21:22:48 +0530

    Day 4: Added Profile work

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -9,10 +9,95 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text(
-        'Profile',
-        ),
-        ),
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: <Widget>[
+        Padding(
+          padding:  EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 0.0),
+          child: Row(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: 
+                NetworkImage('https://avatars2.githubusercontent.com/u/33100167?s=460&v=4'),
+                ),
+                Expanded(
+                child: Column(
+                    children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                   Column(
+                     children: <Widget>[
+                      Text(
+                        '12', 
+                        style: TextStyle(
+                          fontSize: 18.0, 
+                          fontWeight: FontWeight.w600,
+                          ),
+                          ),
+                          Text('posts', 
+                          style: TextStyle(color: Colors.black54),
+                          ),
+                   ],
+                   )
+                    ],
+                    )
+                  ],
+                  ),
+                ),
+                 Column(children: <Widget>[
+                  Row(children: <Widget>[
+                 Column(children: <Widget>[
+                    Text(
+                      '347', 
+                      style: TextStyle(
+                        fontSize: 18.0, 
+                        fontWeight: FontWeight.w600,
+                        ),
+                        ),
+                        Text('followers', style: TextStyle(color: Colors.black54),
+                        ),
+                 ],
+                 )
+                  ],
+                  )
+                ],
+                ), Column(children: <Widget>[
+                  Row(children: <Widget>[
+                 Column(children: <Widget>[
+                    Text(
+                      '667', 
+                      style: TextStyle(
+                        fontSize: 18.0, 
+                        fontWeight: FontWeight.w600,
+                        ),
+                        ),
+                        Text('following', style: TextStyle(color: Colors.black54),
+                        ),
+                 ],
+                 )
+                  ],
+                  ),
+                  Container(
+                    width: 200.0,
+                    child: FlatButton(
+                      onPressed: () => print('Edit Profile'),
+                      textColor: Colors.white,
+                      color: Colors.blue,
+                      child: Text(
+                        'Edit Profile', 
+                        style: TextStyle(fontSize: 18.0),
+                        ),
+                        ),
+                  )
+                ],
+                ),
+          ],
+          ),
+        )
+        ],
+        )
     );
   }
 }
