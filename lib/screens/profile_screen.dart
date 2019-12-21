21980ccf2801a47dca0c0f030a59743d0f31cda4
Subhi Pandey <subhi2012cpi@gmail.com>
2019-12-21 13:53:32 +0530

    Update profile_screen.dart

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -16,41 +16,32 @@ class _ProfileScreenState extends State<ProfileScreen> {
           padding:  EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 0.0),
           child: Row(
             children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: 
-                NetworkImage('https://avatars2.githubusercontent.com/u/33100167?s=460&v=4'),
-                ),
-                Expanded(
-                child: Column(
-                    children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: NetworkImage('https://avatars2.githubusercontent.com/u/33100167?s=460&v=4'),
+            ),
+            Expanded(
+            child: Column(
+                children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
                       children: <Widget>[
-                   Column(
-                     children: <Widget>[
                       Text(
-                        '12', 
-                        style: TextStyle(
-                          fontSize: 18.0, 
-                          fontWeight: FontWeight.w600,
-                          ),
-                          ),
-                          Text('posts', 
-                          style: TextStyle(color: Colors.black54),
-                          ),
-                   ],
-                   )
+                        '52', 
+                      style: TextStyle(
+                        fontSize: 18.0, 
+                        fontWeight: FontWeight.w600,
+                        ),
+                        ),
+                        Text('Posts', style: TextStyle(color: Colors.black54),
+                        ),
                     ],
-                    )
-                  ],
-                  ),
-                ),
-                 Column(children: <Widget>[
-                  Row(children: <Widget>[
-                 Column(children: <Widget>[
-                    Text(
-                      '347', 
+                    ),
+                    Column(
+                      children: <Widget>[
+                      Text('345', 
                       style: TextStyle(
                         fontSize: 18.0, 
                         fontWeight: FontWeight.w600,
@@ -58,16 +49,11 @@ class _ProfileScreenState extends State<ProfileScreen> {
                         ),
                         Text('followers', style: TextStyle(color: Colors.black54),
                         ),
-                 ],
-                 )
-                  ],
-                  )
-                ],
-                ), Column(children: <Widget>[
-                  Row(children: <Widget>[
-                 Column(children: <Widget>[
-                    Text(
-                      '667', 
+                    ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                      Text('667', 
                       style: TextStyle(
                         fontSize: 18.0, 
                         fontWeight: FontWeight.w600,
@@ -75,29 +61,18 @@ class _ProfileScreenState extends State<ProfileScreen> {
                         ),
                         Text('following', style: TextStyle(color: Colors.black54),
                         ),
-                 ],
-                 )
+                    ],
+                    )
                   ],
-                  ),
-                  Container(
-                    width: 200.0,
-                    child: FlatButton(
-                      onPressed: () => print('Edit Profile'),
-                      textColor: Colors.white,
-                      color: Colors.blue,
-                      child: Text(
-                        'Edit Profile', 
-                        style: TextStyle(fontSize: 18.0),
-                        ),
-                        ),
-                  )
-                ],
-                ),
+                )
+              ],
+              ),
+            )
           ],
           ),
         )
-        ],
-        )
+      ],
+      )
     );
   }
 }
